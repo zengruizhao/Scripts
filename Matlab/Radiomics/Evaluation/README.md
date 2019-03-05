af4336dd3269631f371ec1b40cb2960e12dec823
Zengrui Zhao <zzr_nuist@163.com>
2019-03-05 14:25:04 +0800

    modify

--- Matlab/Radiomics/Evaluation/README.md
+++ Matlab/Radiomics/Evaluation/README.md
@@ -1,1 +1,8 @@
 # Decision curve and Calibration curve
+## Decision curve
+![avatar](./decisionCurve.png)
+## Calibration curve
+![avatar](./calibrationCurve.png)
+## Visualization curve
+![avatar](./visualization.png)
+
--- Matlab/Radiomics/Evaluation/demo.m
+++ Matlab/Radiomics/Evaluation/demo.m
@@ -23,3 +23,16 @@ xlabel('Radiomics-Predicted Probability', 'FontSize',16);
 ylabel('Actual Rate of Grade 2', 'FontSize',16);hold off;
 % Decision Curve
 figure;DECISIONCURVE(probs, Y');
+% Visualization
+x = linspace(-40, 40, 1000);
+y = 1./(1 + exp(-x));
+figure;plot(x, y);ylim([-0.1, 1.1]);hold on;
+plot(x, 0.5*ones(1, length(x)), '-.');
+[B, I] = sort(probs);
+sortedLabel = Y(I)';
+for i=1:numel(probs)
+    if sortedLabel(i),color='r';r=i;mkr='o';else,color='b';g=i;mkr='+';end
+    temp(i) = scatter(-log(1/B(i)-1), B(i), color, mkr);
+end
+legend([temp(r), temp(g)],{'1', '2'}, 'Location', 'best');
+hold off;
